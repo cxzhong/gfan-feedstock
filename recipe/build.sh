28104a794d697abc414dae9399e8f390e059f1f6
cxzhong <chenxin.zhong@outlook.com>
2026-03-23 17:01:22 +0800

    Refactor build.sh: Adjust CPPFLAGS, CFLAGS, and CXXFLAGS for improved compilation settings

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export CPPFLAGS="-DNOCDDPREFIX -DDISABLE_COMMENTATOR -I${PREFIX}/include/cddlib $CPPFLAGS"
+export CPPFLAGS="-DNOCDDPREFIX -DDISABLE_COMMENTATOR $CPPFLAGS"
 export LDFLAGS="$LDFLAGS -lcddgmp -lgmp -lm"
-export CFLAGS="-g -fPIC $CFLAGS"
-export CXXFLAGS="-DNOCDDPREFIX -g -fPIC $CXXFLAGS"
+export CFLAGS="-fPIC $CFLAGS"
+export CXXFLAGS="-DNOCDDPREFIX -fPIC -I${PREFIX}/include/cddlib $CXXFLAGS"
 
 if [[ "$target_platform" == "osx-64" ]]; then
   find ${SRC_DIR}/src -type f -print0 | xargs -0 sed -i '' "s/log2/logger2/g"
